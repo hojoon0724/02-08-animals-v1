f7b8c4b1718feb5ccafa8b4efb6ce0f7f7909465
Hojoon Kim <hojoon0724@gmail.com>
2023-12-31 21:55:59 -0800

    Constructor added

---
+++ models/animalsConstructor.js
@@ -0,0 +1,18 @@
+// -----------------------------------------------------
+// Dependencies
+// -----------------------------------------------------
+const mongoose = require("mongoose");
+
+// Create data structure schema for mongoose
+const animalsSchema = new mongoose.Schema({
+  species: { type: String, required: true },
+  extinct: { type: Boolean, required: true },
+  location: { type: String, required: true },
+  lifeExpectancy: { type: Number, required: true },
+});
+
+// Use the mongoose Schema as constructor
+const Animal = mongoose.model("Animal", animalsSchema);
+
+// Export constructor so that it can be used in the app
+module.exports = Animal;
